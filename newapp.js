42b43bb2d1a14f30244be6f63ca90f6ef8e60dac
Praveen Kumar <62791526+kumarpraveen7733@users.noreply.github.com>
2022-02-06 21:37:57 -0800

    commit

--- newapp.js
+++ newapp.js
@@ -30,6 +30,7 @@ app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
 
   // Request headers you wish to allow
+  // Reuest
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
   // Pass to next layer of middleware
